9574715fb9af0bc2e731e7db342cb1185a5afcc4
rishi5544 <67454026+rishi5544@users.noreply.github.com>
2021-07-13 12:59:55 +0530

    Add files via upload

--- accounts/models.py
+++ accounts/models.py
@@ -47,8 +47,8 @@ class Order(models.Model):
 	customer = models.ForeignKey(Customer, null=True, on_delete= models.SET_NULL)
 	product = models.ForeignKey(Product, null=True, on_delete= models.SET_NULL)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	status = models.CharField(max_length=200, null=True, choices=STATUS)
-	note = models.CharField(max_length=1000, null=True)
+	status = models.CharField(max_length=200,default='Pending' ,null=True, choices=STATUS)
+	note = models.CharField(max_length=1000,blank=True, null=True)
 
 	def __str__(self):
 		return self.product.name
